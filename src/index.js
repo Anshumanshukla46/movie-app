58a3750b58310d62a579f7af2765f9cf959a7716
Anshuman Shukla <Anshumanshukla46@gmail.com>
2022-09-26 11:32:30 +0530

    Added Store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+// package import at top
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
+
+// file import 
 import './index.css';
-import App from './components/App';
+import App from './components/App'; // changed app.js location
+
+import movies from './reducers';
+
+// passed reducer
+const store = createStore(movies);
+console.log("store", store);
+
+// empty as in reducer we have a default argument for state be to [] if not present
+console.log("STATE ", store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
